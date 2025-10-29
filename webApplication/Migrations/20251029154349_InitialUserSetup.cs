a9482606a8d6584b567e183e0564d36cfd040d04
Uaekz <ardaumut1995@gmail.com>
2025-10-29 18:46:17 +0300

    User modeli eklendi ve database e bağlandı

---
+++ webApplication/Migrations/20251029154349_InitialUserSetup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialUserSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "TEXT", nullable: false),
+                    name = table.Column<string>(type: "TEXT", nullable: true),
+                    surname = table.Column<string>(type: "TEXT", nullable: true),
+                    phoneNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    email = table.Column<string>(type: "TEXT", nullable: true),
+                    gender = table.Column<bool>(type: "INTEGER", nullable: true),
+                    password = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ webApplication/Models/User.cs
@@ -0,0 +1,13 @@
+namespace webApplication.Models;
+
+public class User
+{
+    public string? id { get; set; }
+    public string? name { get; set; }
+    public string? surname { get; set; }
+    public string? phoneNumber { get; set; }
+    public string? email { get; set; }
+    public bool? gender { get; set; }
+    public string? password { get; set; }
+
+}
