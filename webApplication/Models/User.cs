42ae67f321e82c329d674bc45d9c2d555e19c301
Uaekz <ardaumut1995@gmail.com>
2025-11-27 12:33:09 +0300

    Age eklendi

--- webApplication/Models/User.cs
+++ webApplication/Models/User.cs
@@ -2,12 +2,13 @@ namespace webApplication.Models;
 
 public class User
 {
-    public string? id { get; set; }
+    public string id { get; set; }
     public string? name { get; set; }
     public string? surname { get; set; }
     public string? phoneNumber { get; set; }
     public string? email { get; set; }
-    public bool? gender { get; set; }
-    public string? password { get; set; }
+    public bool gender { get; set; }
+    public string password { get; set; }
+    public int age {get; set;}
 
 }
