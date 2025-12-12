fd23bd84c154ed61d47f3646f50b431bd69cdc5e
Uaekz <ardaumut1995@gmail.com>
2025-12-12 10:42:57 +0300

    Migrations düzeltildi, layout.cshtml düzenlendi.

--- webApplication/Migrations/20251211130602_deneme1.Designer.cs
+++ webApplication/Migrations/20251211130602_deneme1.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using webApplication.Data;
 
@@ -10,9 +11,11 @@ using webApplication.Data;
 namespace webApplication.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251211130602_deneme1")]
+    partial class deneme1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
--- webApplication/Program.cs
+++ webApplication/Program.cs
@@ -12,42 +12,42 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+// using (var scope = app.Services.CreateScope())
+// {
+//     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-    // 1) Add hall
-    if (!db.Halls.Any())
-    {
-        db.Halls.AddRange(
-            new Hall { Name = "PC Hall", Type = "Computer", SeatCount = 20 },
-            new Hall { Name = "Study Hall", Type = "Study", SeatCount = 25 },
-            new Hall { Name = "Reading Hall", Type = "Library", SeatCount = 15 }
-        );
+//     // 1) Add hall
+//     if (!db.Halls.Any())
+//     {
+//         db.Halls.AddRange(
+//             new Hall { Name = "PC Hall", Type = "Computer", SeatCount = 20 },
+//             new Hall { Name = "Study Hall", Type = "Study", SeatCount = 25 },
+//             new Hall { Name = "Reading Hall", Type = "Library", SeatCount = 15 }
+//         );
 
-        db.SaveChanges();
-    }
+//         db.SaveChanges();
+//     }
 
-    // 2) Add Seat
-    if (!db.Seats.Any())
-    {
-        var halls = db.Halls.ToList();
+//     // 2) Add Seat
+//     if (!db.Seats.Any())
+//     {
+//         var halls = db.Halls.ToList();
 
-        foreach (var hall in halls)
-        {
-            for (int i = 1; i <= hall.SeatCount; i++)
-            {
-                db.Seats.Add(new Seat
-                {
-                    SeatNumber = i,
-                    HallId = hall.Id
-                });
-            }
-        }
+//         foreach (var hall in halls)
+//         {
+//             for (int i = 1; i <= hall.SeatCount; i++)
+//             {
+//                 db.Seats.Add(new Seat
+//                 {
+//                     SeatNumber = i,
+//                     HallId = hall.Id
+//                 });
+//             }
+//         }
 
-        db.SaveChanges();
-    }
-}
+//         db.SaveChanges();
+//     }
+// }
 
 
 // Configure the HTTP request pipeline.
